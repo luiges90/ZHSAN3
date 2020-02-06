739471f80133307c3bf22f7b9fb5aafa86e3beca
Peter WONG <luiges90@gmail.com>
2020-02-06 20:53:00 +0800

    按下按鈕才捲動

--- ScenarioScene/MainCamera.gd
+++ ScenarioScene/MainCamera.gd
@@ -19,15 +19,16 @@ func _process(_delta):
 	var mouse_position = get_viewport().get_mouse_position()
 	var viewport_rect = get_viewport_rect()
 	
-	if mouse_position.y < viewport_rect.size.y - bottom_ui_margin:
-		if mouse_position.y < mouse_scroll_margin:
-			offset.y -= camera_speed
-		if mouse_position.y > viewport_rect.size.y - mouse_scroll_margin - bottom_ui_margin:
-			offset.y += camera_speed
-		if mouse_position.x < mouse_scroll_margin:
-			offset.x -= camera_speed
-		if mouse_position.x > viewport_rect.size.x - mouse_scroll_margin:
-			offset.x += camera_speed
+	if Input.is_mouse_button_pressed(BUTTON_LEFT):
+		if mouse_position.y < viewport_rect.size.y - bottom_ui_margin:
+			if mouse_position.y < mouse_scroll_margin:
+				offset.y -= camera_speed
+			if mouse_position.y > viewport_rect.size.y - mouse_scroll_margin - bottom_ui_margin:
+				offset.y += camera_speed
+			if mouse_position.x < mouse_scroll_margin:
+				offset.x -= camera_speed
+			if mouse_position.x > viewport_rect.size.x - mouse_scroll_margin:
+				offset.x += camera_speed
 		
 	if Input.is_action_pressed("ui_up"):
 		offset.y -= camera_speed
