afdc1daf544106da590732c43b3d241c729aaba4
Peter WONG <luiges90@gmail.com>
2020-03-01 18:46:35 +0800

    復原代碼

--- Root.gd
+++ Root.gd
@@ -17,16 +17,6 @@ func _on_NewGame_pressed():
 	get_tree().change_scene("res://Main.tscn")
 
 
-func _on_Other_pressed():
-	$MainMenu.hide()
-	$OtherMenu.show()
-	
-
-func _on_Back_pressed():
-	$OtherMenu.hide()
-	$MainMenu.show()
-
-
 func _on_SaveLoadMenu_file_slot_clicked(mode, path):
 	SharedData.loading_file_path = path
 	get_tree().change_scene("res://Main.tscn")
