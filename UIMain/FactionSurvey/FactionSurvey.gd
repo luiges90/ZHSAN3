b21125baabaaba08bef1f77f37b3a45872f9804c
Peter WONG <luiges90@gmail.com>
2020-08-05 08:38:40 +0800

    勢力簡要放大縮小

--- UIMain/FactionSurvey/FactionSurvey.gd
+++ UIMain/FactionSurvey/FactionSurvey.gd
@@ -1,6 +1,10 @@
 extends TabList
 class_name FactionSurvey
 
+export var full_size = 632
+export var minimize_size = 60
+var hidden = false
+
 func _ready():
 	$Tabs.set_tab_title(0, tr('ARCHITECTURE'))
 	$Tabs.remove_child($Tabs/Tab2)
@@ -12,3 +16,14 @@ func show_data(faction):
 
 func _on_current_faction_set(faction):
 	show_data(faction)
+
+
+func _on_Minimize_pressed():
+	if not hidden:
+		$Tabs.hide()
+		rect_size.y = minimize_size
+		hidden = true
+	else:
+		$Tabs.show()
+		rect_size.y = full_size
+		hidden = false
