88dbb241805e9f7b0ecd97ee9ac3af4dbc31817d
Peter WONG <luiges90@gmail.com>
2020-08-05 21:20:32 +0800

    建築簡表

--- UIMain/FactionSurvey/FactionSurvey.gd
+++ UIMain/FactionSurvey/FactionSurvey.gd
@@ -6,13 +6,26 @@ export var minimize_size = 60
 var hidden = false
 
 func _ready():
-	$Tabs.set_tab_title(0, tr('ARCHITECTURE'))
+	$Tabs.set_tab_title(0, tr('ARCHITECTURE_LIST'))
 	$Tabs.remove_child($Tabs/Tab2)
 	$Tabs.remove_child($Tabs/Tab3)
 	$Tabs.remove_child($Tabs/Tab4)
 
 func show_data(faction):
 	$Title.text = faction.get_name()
+	
+	_populate_architecture_data(faction.get_architectures())
+	
+func _populate_architecture_data(arch_list):
+	var item_list = $Tabs/Tab1/Grid as GridContainer
+	Util.delete_all_children(item_list)
+	item_list.columns = 2
+	item_list.add_child(_title(tr('NAME')))
+	item_list.add_child(_title(tr('PERSON_COUNT')))
+	for arch in arch_list:
+		item_list.add_child(_label(arch.get_name()))
+		item_list.add_child(_label(str(arch.get_workable_persons().size()) + "/" + str(arch.get_workable_persons().size()) + "/" + str(arch.get_faction_persons().size())))
+
 
 func _on_current_faction_set(faction):
 	show_data(faction)
