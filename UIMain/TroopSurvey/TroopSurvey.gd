ab60766f7f19b9c91e8d783c9cb4b098fe8a674e
Peter WONG <luiges90@gmail.com>
2020-08-05 22:20:44 +0800

    部隊簡要勢力

--- UIMain/TroopSurvey/TroopSurvey.gd
+++ UIMain/TroopSurvey/TroopSurvey.gd
@@ -6,6 +6,13 @@ var showing_troop
 func show_data(troop: Troop, mouse_x: int, mouse_y: int):
 	showing_troop = troop
 	
+	var faction = troop.get_belonged_faction()
+	if faction:
+		($Content/Faction/Color as ColorRect).color = faction.color
+		($Content/Faction/Text as Label).text = faction.gname
+	else:
+		($Content/Faction/Color as ColorRect).color = Color(1, 1, 1)
+		($Content/Faction/Text as Label).text = "----"
 	$TitlePanel/Title.text = troop.get_name()
 	
 	$Content/MilitaryKind.text = troop.military_kind.get_name()
