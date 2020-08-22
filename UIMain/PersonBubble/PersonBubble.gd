67845c091a49c8955e4e2cd701fccc42c77f0c13
Peter WONG <luiges90@gmail.com>
2020-08-22 15:07:14 +0800

    修正人物對話框

--- UIMain/PersonBubble/PersonBubble.gd
+++ UIMain/PersonBubble/PersonBubble.gd
@@ -3,7 +3,7 @@ class_name PersonBubble
 
 
 func _on_Timer_timeout():
-	hide()
+	queue_free()
 
 
 func _on_PersonBubble_visibility_changed():
@@ -11,13 +11,19 @@ func _on_PersonBubble_visibility_changed():
 		$Timer.start()
 
 
-func show_bubble(person, scenario_location, text: String):
-	$Portrait.texture = person.get_portrait()
-	
-	var x = scenario_location.get_screen_position().x
-	var y = scenario_location.get_screen_position().y - 88
-	rect_position = Vector2(x, y)
-	$Text.bbcode_text = text
-	show()
+func show_bubble(person, map_object, text: String):
+	var viewing_rect = get_viewport_rect()
+	var x = map_object.get_screen_position().x
+	var y = map_object.get_screen_position().y - 88
+	var bubble_rect = Rect2(x, y, rect_size.x, rect_size.y)
+	if viewing_rect.intersects(bubble_rect):
+		var bubble = duplicate()
+		find_parent("*").add_child(bubble)
+		
+		bubble.find_node('Portrait').texture = person.get_portrait()
+		bubble.rect_position = Vector2(x, y)
+		bubble.find_node('Text').bbcode_text = text
+		
+		bubble.show()
 
 
