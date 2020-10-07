2a381fe600dec5aa48abd0aae81daeedba5329f3
Peter WONG <luiges90@gmail.com>
2020-10-07 20:16:10 +0800

    更新頭像

--- Tools/rename_portrait.py
+++ Tools/rename_portrait.py
@@ -2,7 +2,7 @@ import json
 import os
 
 file_name = '194QXGJ-qh'
-with open(file_name + '/Persons.json', mode='r', encoding='utf-8') as file:
+with open('../Scenarios/' + file_name + '/Persons.json', mode='r', encoding='utf-8') as file:
 	obj = json.loads(file.read())
 
 portrait_dir = '../Images/PersonPortrait/'
@@ -10,7 +10,10 @@ for person in obj:
 	id = person["_Id"]
 	name = person["Surname"] + person["GivenName"]
 	try:
-		os.rename(portrait_dir + name + ".jpg", str(id) + ".jpg")
+		os.rename(portrait_dir + name + ".jpg", portrait_dir + str(id) + ".jpg")
+	except FileExistsError:
+		os.remove(str(id) + ".jpg")
+		os.rename(portrait_dir + name + ".jpg", portrait_dir + str(id) + ".jpg")
 	except FileNotFoundError:
 		print('找不到頭像：' + name)
 	
