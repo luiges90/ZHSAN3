33be762b6ba564cf0b78fd0a9b8559357218615d
Peter WONG <luiges90@gmail.com>
2020-11-13 21:54:55 +0800

    技能習得條件

--- ScenarioUtil.gd
+++ ScenarioUtil.gd
@@ -47,7 +47,10 @@ static func apply_influences(influence_container, in_operation, params: Dictiona
 static func _op_cond(op_not, op_value):
 	return (not op_not and op_value) or (op_not and not op_value)
 
-static func check_conditions(influence_container, params: Dictionary):
+static func __quadratic(level):
+	return 0.25 * level * level + 0.5 * level + 0.25
+
+static func check_conditions(influence_container, params: Dictionary, level = 1):
 	for condition in influence_container.conditions:
 		var operator = Util.dict_try_get(condition, "Operator", [])
 		var op_not = operator.has('not')
@@ -107,15 +110,15 @@ static func check_conditions(influence_container, params: Dictionary):
 						return false
 			'internal_experience_at_least':
 				if params.has('person'):
-					if _op_cond(op_not, params['person'].internal_exp < condition['Value']):
+					if _op_cond(op_not, params['person'].internal_exp < condition['Value'] * __quadratic(level)):
 						return false
 			'combat_experience_at_least':
 				if params.has('person'):
-					if _op_cond(op_not, params['person'].combat_exp < condition['Value']):
+					if _op_cond(op_not, params['person'].combat_exp < condition['Value'] * __quadratic(level)):
 						return false
 			'stratagem_experience_at_least':
 				if params.has('person'):
-					if _op_cond(op_not, params['person'].stratagem_exp < condition['Value']):
+					if _op_cond(op_not, params['person'].stratagem_exp < condition['Value'] * __quadratic(level)):
 						return false
 			'command_at_least':
 				if params.has('person'):
@@ -123,7 +126,7 @@ static func check_conditions(influence_container, params: Dictionary):
 						return false
 			'command_experience_at_least':
 				if params.has('person'):
-					if _op_cond(op_not, params['person'].command_exp < condition['Value']):
+					if _op_cond(op_not, params['person'].command_exp < condition['Value'] * __quadratic(level)):
 						return false
 			'strength_at_least':
 				if params.has('person'):
@@ -131,7 +134,7 @@ static func check_conditions(influence_container, params: Dictionary):
 						return false
 			'strength_experience_at_least':
 				if params.has('person'):
-					if _op_cond(op_not, params['person'].strength_exp < condition['Value']):
+					if _op_cond(op_not, params['person'].strength_exp < condition['Value'] * __quadratic(level)):
 						return false
 			'intelligence_at_least':
 				if params.has('person'):
@@ -139,7 +142,7 @@ static func check_conditions(influence_container, params: Dictionary):
 						return false
 			'intelligence_experience_at_least':
 				if params.has('person'):
-					if _op_cond(op_not, params['person'].intelligence_exp < condition['Value']):
+					if _op_cond(op_not, params['person'].intelligence_exp < condition['Value'] * __quadratic(level)):
 						return false
 			'politics_at_least':
 				if params.has('person'):
@@ -147,7 +150,7 @@ static func check_conditions(influence_container, params: Dictionary):
 						return false
 			'politics_experience_at_least':
 				if params.has('person'):
-					if _op_cond(op_not, params['person'].politics_exp < condition['Value']):
+					if _op_cond(op_not, params['person'].politics_exp < condition['Value'] * __quadratic(level)):
 						return false
 			'glamour_at_least':
 				if params.has('person'):
@@ -155,11 +158,63 @@ static func check_conditions(influence_container, params: Dictionary):
 						return false
 			'glamour_experience_at_least':
 				if params.has('person'):
-					if _op_cond(op_not, params['person'].glamour_exp < condition['Value']):
+					if _op_cond(op_not, params['person'].glamour_exp < condition['Value'] * __quadratic(level)):
 						return false
 			'military_type_experience_at_least':
 				if params.has('person') and params.has('military_type'):
-					if _op_cond(op_not, params['person'].get_military_type_experience(params['military_type']) < condition['Value']):
+					if _op_cond(op_not, params['person'].get_military_type_experience(params['military_type']) < condition['Value'] * __quadratic(level)):
+						return false
+			'popularity_at_least':
+				if params.has('person'):
+					if _op_cond(op_not, params['person'].get_popularity() < condition['Value'] * level):
+						return false
+			'prestige_at_least':
+				if params.has('person'):
+					if _op_cond(op_not, params['person'].get_prestige() < condition['Value'] * level):
+						return false
+			'karma_at_least':
+				if params.has('person'):
+					if _op_cond(op_not, params['person'].get_karma() < condition['Value'] * level):
+						return false
+			'merit_at_least':
+				if params.has('person'):
+					if _op_cond(op_not, params['person'].get_merit() < condition['Value'] * level):
+						return false
+			'ambition_at_least':
+				if params.has('person'):
+					if _op_cond(op_not, params['person'].get_ambition() < condition['Value']):
+						return false
+			'morality_at_least':
+				if params.has('person'):
+					if _op_cond(op_not, params['person'].get_morality() < condition['Value']):
+						return false
+			'troop_damage_dealt_at_least':
+				if params.has('person'):
+					if _op_cond(op_not, params['person'].troop_damage_dealt < condition['Value'] * __quadratic(level)):
+						return false
+			'troop_damage_received_at_least':
+				if params.has('person'):
+					if _op_cond(op_not, params['person'].troop_damage_received < condition['Value'] * __quadratic(level)):
+						return false
+			'arch_damage_dealt_at_least':
+				if params.has('person'):
+					if _op_cond(op_not, params['person'].arch_damage_dealt < condition['Value'] * __quadratic(level)):
+						return false
+			'rout_count_at_least':
+				if params.has('person'):
+					if _op_cond(op_not, params['person'].rout_count < condition['Value'] * __quadratic(level)):
+						return false
+			'routed_count_at_least':
+				if params.has('person'):
+					if _op_cond(op_not, params['person'].routed_count < condition['Value'] * __quadratic(level)):
+						return false
+			'capture_count_at_least':
+				if params.has('person'):
+					if _op_cond(op_not, params['person'].capture_count < condition['Value'] * __quadratic(level)):
+						return false
+			'be_captured_count_at_least':
+				if params.has('person'):
+					if _op_cond(op_not, params['person'].be_captured_count < condition['Value'] * __quadratic(level)):
 						return false
 	return true
 
