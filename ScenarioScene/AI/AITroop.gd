e37166990a75b6cba97ec52cf9d76b53b759f1c6
Peter WONG <luiges90@gmail.com>
2020-11-30 20:06:08 +0800

    調整AI

--- ScenarioScene/AI/AITroop.gd
+++ ScenarioScene/AI/AITroop.gd
@@ -110,7 +110,18 @@ func run_troop(troop, scenario):
 					troop.set_move_order(target)
 					done = true
 			if not done:
-				troop.set_move_order(Util.random_from(movement_area))
+				if randf() < 0.5:
+					var min_dist = 9999999
+					var move_to = null
+					for p in troop._ai_path:
+						for q in movement_area:
+							var dist = Util.m_dist(p, q)
+							if dist < min_dist:
+								move_to = q
+								min_dist = dist
+					troop.set_move_order(move_to)
+				else:
+					troop.set_move_order(Util.random_from(movement_area))
 	
 	assert(troop.current_order != null)
 
