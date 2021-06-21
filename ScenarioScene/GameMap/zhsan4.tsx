55fdfdc71e528e637919ee89dfe68cb3f00f37b1
Peter WONG <luiges90@gmail.com>
2021-06-21 12:53:50 +0800

    update tile map (experiment tiled terrain automap)

---
+++ ScenarioScene/GameMap/zhsan4.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="zhsan4" tilewidth="50" tileheight="50" tilecount="320" columns="20">
+ <image source="ZHSAN4_tile.png" width="1000" height="800"/>
+ <wangsets>
+  <wangset name="Terrain" type="corner" tile="-1">
+   <wangcolor name="Water" color="#0000ff" tile="-1" probability="1"/>
+   <wangcolor name="Grass" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,1,0,2,0,1,0,1"/>
+   <wangtile tileid="1" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="2" wangid="0,1,0,1,0,2,0,1"/>
+   <wangtile tileid="20" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="21" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="22" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="40" wangid="0,2,0,1,0,1,0,1"/>
+   <wangtile tileid="41" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="42" wangid="0,1,0,1,0,1,0,2"/>
+   <wangtile tileid="60" wangid="0,2,0,2,0,2,0,1"/>
+   <wangtile tileid="61" wangid="0,1,0,2,0,2,0,2"/>
+   <wangtile tileid="62" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="80" wangid="0,2,0,2,0,1,0,2"/>
+   <wangtile tileid="81" wangid="0,2,0,1,0,2,0,2"/>
+  </wangset>
+ </wangsets>
+</tileset>
