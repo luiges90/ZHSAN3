1665e1af2adee0f3f4cd1a1e87503e92895e8779
Peter WONG <luiges90@gmail.com>
2021-11-14 22:46:16 +0800

    fix position selector may not click properly

--- ScenarioScene/PositionSelector/PositionSelectItem.gd
+++ ScenarioScene/PositionSelector/PositionSelectItem.gd
@@ -4,26 +4,24 @@ class_name PositionSelectItem
 signal position_selected
 
 var color
+var mouse_in_area = false
 
 func set_color(in_color):
 	color = in_color
 	($Area2D/Sprite as Sprite).modulate = color
 	
 
-func _on_Area2D_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
-		call_deferred("emit_signal", "position_selected")
-		get_tree().set_input_as_handled()
-
-
 func _on_Area2D_mouse_entered():
 	($Area2D/Sprite as Sprite).modulate = color.lightened(0.5)
+	mouse_in_area = true
 
 
 func _on_Area2D_mouse_exited():
 	($Area2D/Sprite as Sprite).modulate = color
+	mouse_in_area = false
 
 
-func _input(event):
-	print(event)
-	pass
+func _unhandled_input(event):
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed and mouse_in_area:
+		call_deferred("emit_signal", "position_selected")
+		get_tree().set_input_as_handled()
